d9150b3b532b7937fb7d8ca485cd58097bd60d38
Iacopo <iacopo.pazzaglia@hotmail.it>
2016-07-11 22:20:45 +0200

    First commit

---
+++ app/src/main/java/pazzaglia/it/moviedb/GridMovieActivity.java
@@ -0,0 +1,13 @@
+package pazzaglia.it.moviedb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GridMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_movie);
+    }
+}
